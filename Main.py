53afa7a44798d015348e1099eb3f44a74c7b5bf2
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-10-27 13:49:37 +0300

    Added command for crearing a console

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
+import os
 import Process_game
+
+os.system('cls')
 
 type_game = input('Введите 1, если хотите играть с другим игроком, и любую другую цифру, если с ботом... ')
 if (type_game == '1'):
